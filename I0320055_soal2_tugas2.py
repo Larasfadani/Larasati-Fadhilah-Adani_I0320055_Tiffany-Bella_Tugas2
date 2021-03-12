c1a963d6bad80f99e3c4f8720350cdb1cb3e760c
L Larasfadani <larasfadani@gmail.com>
2021-03-13 02:17:04 +0700

    Soal 2

---
+++ I0320055_soal2_tugas2.py
@@ -0,0 +1,46 @@
+nama_depan = "Larasati"
+nama_belakang = "Fadhilah Adani"
+nama = nama_depan + " " + nama_belakang
+print("nama = ", nama)
+print("bertype = ", type(nama))
+umur_tahun = "18 tahun"
+umur_bulan = "9 bulan"
+umur_tanggal = "22 hari"
+umur = umur_tahun + " " + umur_bulan + " " + umur_tanggal
+print("umur = ", umur)
+print("bertype = ", type(umur))
+lahir_tanggal = "Tanggal 19"
+lahir_bulan = "Bulan 5"
+lahir_tahun = "Tahun 2002"
+lahir_tempat = "Semarang"
+print("tempat lahir = ", lahir_tempat)
+print("bertype = ", type(lahir_tempat))
+tgl_lahir = lahir_tanggal + " " + lahir_bulan + " " + lahir_tahun
+print("tanggal lahir = ", tgl_lahir)
+print("bertype = ", type(tgl_lahir))
+tinggi_badan = 151.5
+print("tinggi badan = ", tinggi_badan)
+print("bertype = ", type(tinggi_badan))
+berat_badan = 39.8
+print("berat badan = ", berat_badan)
+print("bertype = ", type(berat_badan))
+ukuran_sepatu = 38
+print("ukuran sepatu = ", ukuran_sepatu)
+print("bertype = ", type(ukuran_sepatu))
+alamat_nama_perum = "Fajar Gedongan"
+alamat_blok = "A20"
+alamat_kelurahan = "Gedongan"
+alamat_kecamatan = "Colomadu"
+alamat_kabupaten = "Karanganyar"
+alamat = alamat_nama_perum + " " + "blok" + " " + alamat_blok + "," + " " + alamat_kelurahan + "," + " " + alamat_kecamatan + "," + " " + alamat_kabupaten
+print("alamat = ", alamat)
+print("bertype = ", type(alamat))
+
+print("Biodata saya")
+print("Nama saya adalah", nama)
+print("Saya berumur", umur)
+print("Saya lahir pada", tgl_lahir)
+print("Saya memiliki tinggi badan", tinggi_badan, "cm")
+print("Sedangkan, berat badan saya", berat_badan, "kg")
+print("Ukuran sepatu saya sekitar", ukuran_sepatu)
+print("Saya tinggal di", alamat)
