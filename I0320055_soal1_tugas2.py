907114bde09dbfcb6b58c779553ef773225a0050
L Larasfadani <larasfadani@gmail.com>
2021-03-12 21:18:13 +0700

    Exercise 2.1-2.4 dan Soal 1-2

---
+++ I0320055_soal1_tugas2.py
@@ -0,0 +1,41 @@
+#Menghitung luas persergi panjang
+print("Menghitung Luas Persegi Panjang")
+p = 15
+l = 6
+luas_persegi_panjang = p * l
+print("Panjang persegi panjang = ", p)
+print("Lebar persegi panjang = ", l)
+print("Hitunglah luas persegi panjang!")
+print("Luas persegi panjang adalah = ", luas_persegi_panjang)
+
+#Menghitung luas lingkaran
+print("Menghitung Luas Lingkaran")
+r = 5
+phi = 3.14
+luas_lingkaran = phi * (r ** 2)
+print("Jari-jari lingkaran = ", r)
+print("Phi = ", phi)
+print("Hitunglah luas lingkaran!")
+print("Luas lingkaran adalah = ", luas_lingkaran)
+
+#Menghitung luas kubus
+print("Menghitung Luas Kubus")
+s = 3
+luas_kubus = 6 * (s ** 2)
+print("Sisi kubus adalah = ", s)
+print("Hitunglah luas kubus!")
+print("Luas kubus adalah = ", luas_kubus)
+
+#Menghitung konversi suhu celcius ke fahrenheit
+print("Mengonversi Suhu dari Celcius ke Fahrenheit")
+celcius = float(input("Suhu dalam celcius = "))
+fahrenheit = ((9/5) * celcius) + 32
+print("Suhu adalah", celcius,"celcius")
+print("Suhu dalam fahrenheit adalah = ", fahrenheit, "fahrenheit")
+
+#Menghitung konversi suhu reamur ke kelvin
+print("Mengonversi Suhu dari Reamur ke Kelvin")
+reamur = (4/5) * celcius
+print("Suhu dalam reamur = ", reamur)
+kelvin = ((5/4 * reamur) + 273)
+print("Suhu dalam kelvin = ", kelvin)
